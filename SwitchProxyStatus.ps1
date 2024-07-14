35361a413e3847588c6079be505191411e8d1030
ChasingBoy <409562143@qq.com>
2024-07-15 06:29:17 +0800

    Add files via upload

---
+++ SwitchProxyStatus.ps1
@@ -0,0 +1,13 @@
+﻿$ipconfig = ipconfig /all
+
+$dhcpServerIPv4 = ($ipconfig | Select-String "DHCP Server" | Select-Object -First 1).ToString().Split(":")[1].Trim()
+
+
+# 切换手动配置的 Internet 代理状态
+$proxySettings = Get-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings"
+if ($proxySettings.ProxyEnable -eq 1) {
+    Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings" -Name "ProxyEnable" -Value 0
+} else {
+    Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings" -Name "ProxyEnable" -Value 1
+    Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings" -Name "ProxyServer" -Value "$dhcpServerIPv4`:10809"
+}
---
+++ 打开代理.ps1
@@ -0,0 +1,6 @@
+﻿$ipconfig = ipconfig /all
+
+$dhcpServerIPv4 = ($ipconfig | Select-String "DHCP Server" | Select-Object -First 1).ToString().Split(":")[1].Trim()
+
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings" -Name "ProxyEnable" -Value 1
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings" -Name "ProxyServer" -Value "$dhcpServerIPv4`:10809"
