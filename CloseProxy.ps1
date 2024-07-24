d84912da8ca45415a62167494690a88919703333
ChasingBoy <409562143@qq.com>
2024-07-24 15:44:33 +0800

    clear github config

--- CloseProxy.ps1
+++ CloseProxy.ps1
@@ -1,1 +1,3 @@
-﻿Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings" -Name "ProxyEnable" -Value 0
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings" -Name "ProxyEnable" -Value 0
+git config --global --unset http.proxy
+git config --global --unset https.proxy
