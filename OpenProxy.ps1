a5621c4a999f88e146fe8652488ea32f805c86df
ChasingBoy <409562143@qq.com>
2024-07-24 15:45:04 +0800

    set github proxy

--- OpenProxy.ps1
+++ OpenProxy.ps1
@@ -1,6 +1,10 @@
-﻿$ipconfig = ipconfig /all
+$ipconfig = ipconfig /all
 
 $dhcpServerIPv4 = ($ipconfig | Select-String "DHCP Server" | Select-Object -First 1).ToString().Split(":")[1].Trim()
 
 Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings" -Name "ProxyEnable" -Value 1
 Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings" -Name "ProxyServer" -Value "$dhcpServerIPv4`:10809"
+
+$git_proxy = "$($dhcpServerIPv4):10809"
+git config --global http.proxy $git_proxy
+git config --global https.proxy $git_proxy
